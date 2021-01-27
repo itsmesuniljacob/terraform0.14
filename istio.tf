b6c1801cdf6d9efb789c73945ccad941fbc2a3f8
Sunil.Jacob <sunil1405@gmail.com>
2021-01-27 10:33:14 +0530

    Deleted istio.tf file

--- istio.tf
+++
@@ -1,7 +0,0 @@
-# resource "helm_release" "istio-operator" {
-#   name       = "istio-operator"
-#   repository = "${data.helm_repository.istio.metadata.0.name}"
-#   chart      = "istio-init"
-#   version    = "${var.istio_version}"
-#   namespace  = "${var.istio_namespace}"
-# }
